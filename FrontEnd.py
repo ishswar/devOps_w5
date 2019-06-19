ec4a94529b2683bead50de64d5d094f5bb8b637e
ishswar <pranay.shah@gmail.com>
2019-06-19 14:45:22 -0700

    Front end nameko RPC/HTTP Service ; accepts HTTP request with file name as input and makes a RPC call to backend and get file content over RPC

--- FrontEnd.py
+++ FrontEnd.py
@@ -1,4 +1,5 @@
 from nameko.standalone.rpc import ClusterRpcProxy
+from nameko.web.handlers import http
 
 config = {
     'AMQP_URI': "amqp://guest:guest@192.168.33.76:7600//"  # e.g. "pyamqp://guest:guest@localhost"
@@ -6,6 +7,20 @@ config = {
 
 #n.rpc.greeting_service.hello
 
-with ClusterRpcProxy(config) as cluster_rpc:
-    rest = cluster_rpc.backEnd_service.getFile("testfile.text")
-    print("File Content of file testfile.text: \n####################################################\n"+ rest + "\n####################################################")
+class HttpService:
+	name = "http_service"
+
+	@http('GET', '/get/<string:value>')
+	def get_method(self, request, value):
+		print("Input is "+ value)
+		with ClusterRpcProxy(config) as cluster_rpc:
+			rest = cluster_rpc.backEnd_service.getFileContents(value)
+			print("File Content of file testfile1.text: \n@@@@@@@@@@@@@@@@@##############################\n"+ rest + "\n####################################################")
+			if rest.startswith( '~' ):
+			 return 404, "File "+ value + " NOT_FOUND"
+			else:
+			 return rest
+
+# with ClusterRpcProxy(config) as cluster_rpc:
+#     rest = cluster_rpc.backEnd_service.getFile("testfile.text")
+#     print("File Content of file testfile.text: \n####################################################\n"+ rest + "\n####################################################")
